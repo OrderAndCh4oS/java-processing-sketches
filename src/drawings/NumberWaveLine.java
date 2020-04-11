a99cb7273a58ea96ac7e9049e9d751bd44881a29
Sarcoma <sarcoma@live.co.uk>
2020-04-11 16:24:44 +0100

    clean number wave line

--- src/drawings/NumberWaveLine.java
+++ src/drawings/NumberWaveLine.java
@@ -26,39 +26,34 @@ public class NumberWaveLine extends Sketch {
         _colours.getColours().forEach((name, colour) -> {
             PFont font = createFont("ModernTwoSxtnITCStd-Bold", 1200);
             textFont(font);
-            String[] numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+            String[] numbers = {"1", "2", "3", "4", "5"};
             for (String n : numbers) {
                 background(0xff000000);
                 fill(0xffffffff);
                 textAlign(CENTER);
                 text(n, _width/2, 908);
                 PImage img = get(0, 0, (int) _width, (int) _height);
-                int step = 5;
-                float lineWidth = random(12, 26);
-                float padding = random(18, 22);
+                int step = 6;
+                float lineWidth = random(20, 30);
+                float padding = random(15, 20);
                 float wave = 0.12f;
-                int bg = colour.rand();
-                int lineColour = colour.rand();
                 background(colour.rand());
+                drawDepth(colour.bg(), 0.8f, 0.2f);
                 strokeWeight(1);
                 strokeCap(ROUND);
-                for (int x = 0; x < _width; x += lineWidth - padding) {
-                    stroke(colour.rand());
+                stroke(colour.bg());
+                for (int x = -30; x < _width; x += lineWidth - padding) {
                     float a = 0;
                     int offsetY = (x % step);
-                    for (int y = 0; y < _width; y += step) {
+                    for (int y = -10; y < _height; y += step) {
                         PImage section = img.get(x, y, step, step);
                         float shade = extractShadeFromImage(section.pixels);
                         float dX = sin(a) * 40;
                         float newX = dX - dX / 2;
-                        if (shade < 0.5) {
-                            lineColour = colour.rand();
-                            while (lineColour == bg) {
-                                lineColour = colour.rand();
-                            }
-                            stroke(lineColour);
+                        if (shade > 0.5) {
+                            strokeWeight(2);
                         } else {
-                            stroke(colour.bg());
+                            strokeWeight(1);
                         }
                         line(
                                 x + newX + step/2f,
