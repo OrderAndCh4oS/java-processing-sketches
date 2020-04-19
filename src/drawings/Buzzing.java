b1f1bca8b739be387b4779ae366e2d371bf556e1
Sarcoma <sarcoma@live.co.uk>
2020-04-19 22:43:57 +0100

    Add buzzing

--- src/drawings/Buzzing.java
+++ src/drawings/Buzzing.java
@@ -17,39 +17,47 @@ public class Buzzing extends Sketch {
     @Override
     public void settings() {
         _save = true;
-        super.settings(1024, 1024, P3D);
+        super.settings(1024, 1024, P2D);
         smooth(8);
     }
 
     @Override
     public void sketch() {
         _colours.getColours().forEach((name, colour) -> {
-            background(colour.bg());
+            int bg = colour.rand();
+            background(bg);
             pushMatrix();
             translate(_width / 2, _height / 2);
-            int iterations = 100;
-            stroke(colour.rand());
-            strokeWeight(3);
-            noFill();
+            int iterations = 124;
+            int c1 = colour.bg();
             for (int i = 0; i < iterations; i++) {
                 ArrayList<Point> points = new ArrayList<>();
-                for (int j = 0; j < 360; j++) {
-                    float radius = (_width / iterations) * i;
-                    float amplitude = map(i, 0, iterations, 1, 18);
-                    float angle = (j / 360f) * TAU;
-                    Point p = sineAroundCircle(0, 0, radius, amplitude, angle, 30);
-                    points.add(p);
-                }
-                beginShape();
-                for (Point p : points) {
-                    curveVertex(p.x(), p.y());
-                }
-                endShape(CLOSE);
+                float radius = ((_width * 0.75f) / iterations) * i;
+                float amplitude = map(i, 0, iterations, 1, 24);
+                float frequency = 24;
+                drawWaveLoop(c1, points, radius, amplitude, frequency);
             }
-
             popMatrix();
             save("buzzing", name);
         });
+    }
+
+    private void drawWaveLoop(int c1, ArrayList<Point> points, float radius, float amplitude, float frequency) {
+        for (int j = 0; j < 360; j++) {
+            float angle = (j / 360f) * TAU;
+            Point p = sineAroundCircle(0, 0, radius, amplitude, angle, frequency);
+            points.add(p);
+        }
+
+        stroke(c1);
+        strokeWeight(0.5f);
+        noFill();
+
+        beginShape();
+        for (Point p : points) {
+            curveVertex(p.x(), p.y());
+        }
+        endShape(CLOSE);
     }
 
     public Point sineAroundCircle(float cx, float cy, float radius, float amplitude, float angle, float frequency) {
