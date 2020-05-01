023794ed9eafbb0af98eae3474c53af7fc3f823a
Sarcoma <sarcoma@live.co.uk>
2020-05-01 20:27:06 +0100

    HSL mostly working

--- src/drawings/Saturation.java
+++ src/drawings/Saturation.java
@@ -16,7 +16,7 @@ public class Saturation extends Sketch {
     @Override
     public void settings() {
         _save = true;
-        super.settings(1024, 1024, P3D);
+        super.settings(1024, 1024, P2D);
         smooth(8);
     }
 
@@ -25,15 +25,15 @@ public class Saturation extends Sketch {
         _colours.getColours().forEach((name, colour) -> {
             background(colour.bg());
             noFill();
-            for (int i = 0; i < 240; i++) {
+            for (int i = 0; i < 60; i++) {
                 float x1 = -20;
                 float y1 = random(_height + 200) - 10;
                 float x2 = _width + 10;
                 float y2 = random(_height + 200) - 10;
-                float x1h = x1 + random(225);
-                float y1h = y1 + random(225);
-                float x2h = x2 - random(225);
-                float y2h = y2 - random(225);
+                float x1h = x1 + random(40,100);
+                float y1h = y1 + random(40,100);
+                float x2h = x2 - random(40,100);
+                float y2h = y2 - random(40,100);
                 Point a = new Point(x1, y1);
                 Point d = new Point(x2, y2);
                 Point c = new Point(x1h, y1h);
@@ -43,10 +43,10 @@ public class Saturation extends Sketch {
                 for (float t = 0; t < 1; t += 0.001) {
                     Point p = cubicBezier(a, b, c, d, t);
                     c1.setS((int) map(t, 0, 1, 0, 100));
-                    fill(c1.getRgb() * 255);
+                    fill(c1.getRgba());
                     pushMatrix();
                     translate(p.x(), p.y());
-                    float size = 80;
+                    float size = 36;
                     rect(0, 0, size, size);
                     popMatrix();
                 }
